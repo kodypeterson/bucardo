a1c4b7527d141e0a20c05ecb3a593089df2cb594
Kody Peterson <kodypeterson91@gmail.com>
2023-10-25 11:49:57 -0400

    Missed in revert

--- docker-start-up.sh
+++ docker-start-up.sh
@@ -1,23 +1,6 @@
-#!/bin/bash
-set -e
+# Call check bucardo.json in infinity loop in background
+while true; do sleep 10; /bin/bash apply-bucardo-config.sh;   done &
 
-
-# Function to install and configure Bucardo
-install_bucardo() {
-    # Check if the bucardo user already exists, if not create it
-    if [[ "$(psql -U postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='bucardo'")" != "1" ]]; then
-        psql -U postgres -c "CREATE ROLE bucardo WITH SUPERUSER LOGIN PASSWORD 'bucardo';"
-    fi
-
-    # Check if the bucardo database already exists, if not create it
-    if [[ "$(psql -U postgres -tAc "SELECT 1 FROM pg_database WHERE datname='bucardo'")" != "1" ]]; then
-        psql -U postgres -c "CREATE DATABASE bucardo OWNER bucardo;"
-        psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE bucardo TO bucardo;"
-    fi
-
-    # Initialize Bucardo
-    bucardo install --batch
-}
-
-# Call the functions
-install_bucardo
+# Call entrypoint and cmd of the postgres docker image
+# It is blocking command, so it makes container to run permanently
+docker-entrypoint.sh postgres
